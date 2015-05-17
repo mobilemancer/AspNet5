5e3a8e19fad83588c0dd9f581d3de1ba3160d5a8
mobilemancer <andrkarl@gmail.com>
2015-05-18 01:04:20 +0200

    First

---
+++ ModularPipeline/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace ModularPipeline
+{
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            //app.UseStaticFiles();
+
+            //app.UseWelcomePage();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello SweNug!");
+            });
+        }
+    }
+}
