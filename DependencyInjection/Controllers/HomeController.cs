ddcaf0cfed494bf3a366d1c72d133402b2cb2dcd
mobilemancer <andrkarl@gmail.com>
2015-05-19 00:23:52 +0200

    Added DI solution

---
+++ DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using DependencyInjection.Services;
+using Microsoft.AspNet.Mvc;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private GreeterService gs1;
+
+        [Activate]
+        public GreeterService GreeterService { get; set; }
+
+
+        public HomeController(GreeterService greeter)
+        {
+            gs1 = greeter;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.greeter1 = gs1;
+            ViewBag.greeter2 = GreeterService;
+
+            return View();
+        }
+    }
+}
---
+++ DependencyInjection/Services/GreeterService.cs
@@ -0,0 +1,17 @@
+﻿namespace DependencyInjection.Services
+{
+    public class GreeterService
+    {
+        static int counter;
+
+        public GreeterService()
+        {
+            counter = 5;
+        }
+
+        public string Greet()
+        {
+            return "no " + counter++;
+        }
+    }
+}
