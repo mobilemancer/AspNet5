afcc63ebb4a89479ff87911725e93c411811b5a0
mobilemancer <andrkarl@gmail.com>
2015-05-21 00:23:03 +0200

    Added TestingWithxUnit project

---
+++ TestingWithxUnit/GreeterServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using TagHelpers.Services;
+using Xunit;
+
+namespace TestingWithxUnit
+{
+    public class GreeterServiceTests
+    {
+        [Fact]
+        public void GreetTest()
+        {
+            var greeter = new GreeterService();
+
+            Assert.Equal(greeter.Greet(), "no 1");
+        }
+
+        [Fact]
+        public void RiggedGreetTest()
+        {
+            var greeter = new GreeterService();
+
+            Assert.Equal(greeter.RiggedGreet(42), "no 42");
+        }
+
+        [Fact]
+        public void GreetNameTest()
+        {
+            var greeter = new GreeterService();
+
+            Assert.Equal(greeter.GreetName("SweNug"), "Hello SweNug from the Greeter tag!");
+
+        }
+
+
+    }
+}
